b5d2f532343d6142cbe49b8cf6b5530b5fdd42b5
sanchayancse <sanchayandas1996@gmail.com>
2024-11-20 23:26:10 +0530

    code update for GET,POST,PATCH,DELETE method of user route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  /*
+    GET /users
+    GET /users/:id
+    POST /users
+    PATCH /users/:id
+    DELETE /users/:id
+  */
+  @Get()
+  findAll(@Query('role') role?: 'Intern' | 'Engineer' | 'Admin') {
+    return 'This action returns all users';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post()
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  create(@Body() user: {}) {
+    return user;
+  }
+
+  @Patch(':id')
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  update(@Param('id') id: string, @Body() userUpdate: {}) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return { id };
+  }
+}
